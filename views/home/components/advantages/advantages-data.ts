a0186461384eab525264fbf09bf738681e9c4c1f
git-marcopitra <marcomorete73@hotmail.com>
2023-04-19 02:53:10 +0100

    fix: advantages position and slick

--- views/home/components/advantages/advantages-data.ts
+++ views/home/components/advantages/advantages-data.ts
@@ -5,17 +5,53 @@ import {
   StepsIllustration,
 } from './advantages-illustrations';
 
-export const CONTENT_LIST: ReadonlyArray<AdvantagesCardProps> = [
+export const ADVANTAGE_LIST: ReadonlyArray<AdvantagesCardProps> = [
   {
-    position: 'first',
+    name: 'rewards',
     Illustration: StepsIllustration,
   },
   {
-    position: 'second',
+    name: 'profits',
     Illustration: BlocksIllustration,
   },
   {
-    position: 'third',
+    name: 'fees',
+    Illustration: CoinsIllustration,
+  },
+  {
+    name: 'rewards',
+    Illustration: StepsIllustration,
+  },
+  {
+    name: 'profits',
+    Illustration: BlocksIllustration,
+  },
+  {
+    name: 'fees',
     Illustration: CoinsIllustration,
   },
 ];
+
+export const slickSettings = {
+  speed: 500,
+  dots: false,
+  arrows: false,
+  autoplay: false,
+  infinite: false,
+  slidesToShow: 3,
+  centerMode: false,
+  slidesToScroll: 0,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        autoplay: true,
+        infinite: true,
+        slidesToShow: 3,
+        centerMode: true,
+        slidesToScroll: 1,
+        centerPadding: '0px',
+      },
+    },
+  ],
+};
--- views/home/components/advantages/advantages-illustrations.tsx
+++ views/home/components/advantages/advantages-illustrations.tsx
@@ -2,5 +2,7 @@ import { Box } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
 export const StepsIllustration: FC = () => <Box></Box>;
+
 export const BlocksIllustration: FC = () => <Box></Box>;
+
 export const CoinsIllustration: FC = () => <Box></Box>;
