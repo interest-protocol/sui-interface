63d39708bdd6f9ae23bac9b8992c20e5f829b752
git-marcopitra <marcomorete73@hotmail.com>
2023-04-20 22:58:06 +0100

    chore: add hover effect to button

--- views/institutional/components/hero/hero-call-to-action.tsx
+++ views/institutional/components/hero/hero-call-to-action.tsx
@@ -1,6 +1,9 @@
 import { Box, Button, Typography } from '@interest-protocol/ui-kit';
+import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 import { FC } from 'react';
+
+import { Routes, RoutesEnum } from '@/constants';
 
 const HeroCallToAction: FC = () => {
   const t = useTranslations();
@@ -19,9 +22,11 @@ const HeroCallToAction: FC = () => {
       >
         {t('landingPage.hero.description')}
       </Typography>
-      <Button my="4xl" variant="filled">
-        {t('landingPage.hero.button')}
-      </Button>
+      <Link href={Routes[RoutesEnum.DEX]}>
+        <Button my="4xl" variant="filled">
+          {t('landingPage.hero.button')}
+        </Button>
+      </Link>
     </Box>
   );
 };
