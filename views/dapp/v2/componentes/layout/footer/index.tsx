c22965bbf8467dc63c26670fae37d326bf7e0ea8
Okusindja de Almeida <okusindja@gmail.com>
2023-04-24 14:32:31 +0100

    feat: add footer

--- views/dapp/v2/componentes/layout/footer/index.tsx
+++ views/dapp/v2/componentes/layout/footer/index.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
+import Socials from './socials';
 
-const Footer: FC = () => <Box></Box>;
+const Footer: FC = () => <Socials />;
 
 export default Footer;
