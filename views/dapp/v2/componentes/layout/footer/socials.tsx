5dfad27f35598da0c8592d95cc89a4ab4b4412ad
Okusindja de Almeida <okusindja@gmail.com>
2023-04-28 02:09:15 +0100

    chore: changing icons color

--- views/dapp/v2/componentes/layout/footer/socials.tsx
+++ views/dapp/v2/componentes/layout/footer/socials.tsx
@@ -1,4 +1,4 @@
-import { Box, Motion } from '@interest-protocol/ui-kit';
+import { Box, Motion, Theme, useTheme } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 import { v4 } from 'uuid';
 
@@ -6,25 +6,29 @@ import { SOCIAL_MEDIAS_ALTERNATIVE } from '@/constants';
 
 import { SocialWrapperProps } from './social.types';
 
-const SocialWrapper: FC<SocialWrapperProps> = ({ Icon, link }) => (
-  <Motion
-    as="a"
-    color="primary"
-    cursor="pointer"
-    whileHover={{
-      translateY: -5,
-      filter: 'hue-rotate(15deg) brightness(1.5) opacity(0.95)',
-    }}
-    transition={{ duration: 0.3 }}
-    {...{ href: link, target: '_blank', rel: 'noreferrer' }}
-  >
-    <Icon width="1.375rem" maxWidth="1.375rem" maxHeight="1.375rem" />
-  </Motion>
-);
+const SocialWrapper: FC<SocialWrapperProps> = ({ Icon, link }) => {
+  const { dark } = useTheme() as Theme;
+  return (
+    <Motion
+      as="a"
+      color={dark ? 'background' : 'accentSoft'}
+      cursor="pointer"
+      whileHover={{
+        translateY: -5,
+        filter: 'hue-rotate(15deg) brightness(1.5) opacity(0.95)',
+      }}
+      transition={{ duration: 0.3 }}
+      {...{ href: link, target: '_blank', rel: 'noreferrer' }}
+    >
+      <Icon width="1.375rem" maxWidth="1.375rem" maxHeight="1.375rem" />
+    </Motion>
+  );
+};
 
 const Socials: FC = () => (
   <Box
     mx="auto"
+    gap="1.5rem"
     width="100%"
     pt="6.25rem"
     pb="1.875rem"
@@ -33,7 +37,6 @@ const Socials: FC = () => (
     position="relative"
     justifyContent="center"
     background="background"
-    gap={['1.5rem', '1.5rem', '2.5rem']}
   >
     {SOCIAL_MEDIAS_ALTERNATIVE.map(({ Logo, link }) => (
       <SocialWrapper Icon={Logo} link={link} key={v4()} />
