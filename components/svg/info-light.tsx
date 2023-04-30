380cd78378ddfe1b169a06bbfafd8b2ce2821694
Okusindja de Almeida <okusindja@gmail.com>
2023-04-30 02:21:08 +0100

    feat: add first section of lending

--- components/svg/info-light.tsx
+++ components/svg/info-light.tsx
@@ -5,13 +5,24 @@ import { SVGProps } from './svg.types';
 const InfoLight: FC<SVGProps> = ({ maxHeight, maxWidth, ...props }) => (
   <svg
     style={{ maxWidth: maxWidth, maxHeight: maxHeight }}
-    viewBox="0 0 31 31"
+    viewBox="0 0 16 16"
     fill="none"
     {...props}
   >
-    <circle cx="15.5" cy="15.5" r="15" stroke="currentColor" />
     <path
-      d="M14.168 28V11.2H16.44V28H14.168ZM15.32 7.488C14.8507 7.488 14.456 7.328 14.136 7.008C13.816 6.688 13.656 6.304 13.656 5.856C13.656 5.408 13.816 5.03466 14.136 4.736C14.456 4.416 14.8507 4.256 15.32 4.256C15.7893 4.256 16.184 4.40533 16.504 4.704C16.824 5.00267 16.984 5.376 16.984 5.824C16.984 6.29333 16.824 6.688 16.504 7.008C16.2053 7.328 15.8107 7.488 15.32 7.488Z"
+      d="M8 14C11.3137 14 14 11.3137 14 8C14 4.68629 11.3137 2 8 2C4.68629 2 2 4.68629 2 8C2 11.3137 4.68629 14 8 14Z"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M7.5 7.5H8V11H8.5"
+      stroke="currentColor"
+      strokeLinecap="square"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M7.875 6C8.28921 6 8.625 5.66421 8.625 5.25C8.625 4.83579 8.28921 4.5 7.875 4.5C7.46079 4.5 7.125 4.83579 7.125 5.25C7.125 5.66421 7.46079 6 7.875 6Z"
       fill="currentColor"
     />
   </svg>
--- views/dapp/v2/lending-protocol/index.tsx
+++ views/dapp/v2/lending-protocol/index.tsx
@@ -1,7 +1,12 @@
 import { FC } from 'react';
 
 import { Layout } from '../componentes';
+import LendBalanceInfo from '../componentes/lend-balance-info';
 
-const LendingProtocol: FC = () => <Layout></Layout>;
+const LendingProtocol: FC = () => (
+  <Layout>
+    <LendBalanceInfo />
+  </Layout>
+);
 
 export default LendingProtocol;
