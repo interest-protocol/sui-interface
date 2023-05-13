1c24bc7373da30b01af4d44d36d494c8ae89689c
Okusindja de Almeida <91469357+okusindja@users.noreply.github.com>
2023-05-13 01:01:51 +0100

    feat: add hero section (#209)
    * feat: hero svg to fix
    
    * chore: change illustration in the component
    
    * fix: change the svg and solve svg compatibility errors
    
    * chore: add en-US translations
    
    * chore: change and update pt-BR translations
    
    * chore: change and update pt-PT translations
    
    * build: fix
    
    * build: fix
    
    ---------
    
    Co-authored-by: git-marcopitra <marcomorete73@hotmail.com>

--- views/institutional/liquidity/index.tsx
+++ views/institutional/liquidity/index.tsx
@@ -2,11 +2,12 @@ import { Box } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
 import { Layout } from '../components';
+import LiquidityHero from '../components/liquidity-hero';
 
 const Liquidity: FC = () => (
   <Layout noContent>
     <Box bg="background" color="text" flex="1" textAlign="center">
-      Liquidity
+      <LiquidityHero />
     </Box>
   </Layout>
 );
