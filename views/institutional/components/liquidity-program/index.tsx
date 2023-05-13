7b92f2b2a0615f69a6c3e5a1cf7c53cdd0372a49
git-marcopitra <marcomorete73@hotmail.com>
2023-05-14 00:02:57 +0100

    fix: review

--- views/institutional/components/liquidity-program/index.tsx
+++ views/institutional/components/liquidity-program/index.tsx
@@ -1,8 +1,8 @@
 import { Box } from '@interest-protocol/ui-kit';
-import { FC } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { v4 } from 'uuid';
 
-import { useHorizontalScroll } from '@/hooks';
+import useEventListener from '@/hooks/use-event-listener';
 
 import LiquidityProgramTitle from './liquidity-program-title';
 import LiquiditySimpleInformation from './liquidity-simple-informations';
@@ -11,30 +11,44 @@ import { POOL_PROVIDERS_LIST } from './pool-provider.data';
 import PoolProviderCard from './pool-provider-card';
 
 const LiquidityProgram: FC = () => {
-  const scrollRef = useHorizontalScroll();
+  const ref = useRef<HTMLDivElement>(null);
+  const [mouseOver, setMouseOver] = useState(false);
+
+  useEffect(() => {
+    console.log('>> mouseOver :: ', mouseOver);
+  }, [mouseOver]);
+
+  const onWheel = (e: WheelEvent) => {
+    if (ref.current) {
+      e.stopImmediatePropagation();
+      ref.current.scrollLeft += e.deltaY;
+    }
+  };
+
+  useEventListener('wheel', onWheel as any, false, ref.current!);
 
   return (
-    <Box bg="background" py="4xl">
-      <Box variant="container">
-        <LiquidityProgramTitle />
-        <Box
-          width="100%"
-          ref={scrollRef}
-          overflow="hidden"
-          marginBottom="3.75rem"
-          scrollbarColor="background"
-          gridColumn={['1/23', '1/23', '1/16', '1/23']}
-        >
-          <Box display="flex" gap="s">
-            {POOL_PROVIDERS_LIST.map((poolProvider) => (
-              <PoolProviderCard key={v4()} {...poolProvider} />
-            ))}
-          </Box>
+    <Box variant="container">
+      <LiquidityProgramTitle />
+      <Box
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        ref={ref}
+        width="100%"
+        gridColumn="1/-1"
+        onMouseEnter={() => setMouseOver(true)}
+        onMouseLeave={() => setMouseOver(false)}
+      >
+        <Box gap="s" display="flex" width="100%">
+          {POOL_PROVIDERS_LIST.map((poolProvider) => (
+            <PoolProviderCard key={v4()} {...poolProvider} />
+          ))}
         </Box>
-        <LiquiditySimpleInformation />
-        <LiquidityWarning />
       </Box>
+      <LiquiditySimpleInformation />
+      <LiquidityWarning />
     </Box>
   );
 };
+
 export default LiquidityProgram;
