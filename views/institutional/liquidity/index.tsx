2af6270f4af9c5093b3e1dd4ea2aa3dd80a5b45b
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2023-05-14 10:33:54 +0100

    :sparkles: Then section (#235)
    * chore: wip
    
    * feat: :sparkles: cards implemented
    
    ---------
    
    Co-authored-by: KipandaJr <antoniokipandacardoso@gmail.com>

--- views/institutional/liquidity/index.tsx
+++ views/institutional/liquidity/index.tsx
@@ -4,12 +4,14 @@ import { FC } from 'react';
 import { Layout } from '../components';
 import FirstThingFirst from '../components/first-thing-first-section';
 import LiquidityHero from '../components/liquidity-hero';
+import ThenSection from '../components/then';
 
 const Liquidity: FC = () => (
   <Layout noContent>
     <Box bg="background" color="text" flex="1" textAlign="center">
       <LiquidityHero />
       <FirstThingFirst />
+      <ThenSection />
     </Box>
   </Layout>
 );
