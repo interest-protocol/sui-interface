bbebc7268aef2fc1a6cb0fadc098cda16fb77d7e
Kipas Jr <34079870+KipandaJr@users.noreply.github.com>
2023-05-15 09:46:16 +0100

    feat: :lipstick: change text in title and add tutorial button (#249)
    Co-authored-by: git-marcopitra <marcomorete73@hotmail.com>

--- views/institutional/components/first-thing-first-section/action-button.tsx
+++ views/institutional/components/first-thing-first-section/action-button.tsx
@@ -5,42 +5,21 @@ import { FC } from 'react';
 import { Routes, RoutesEnum } from '@/constants';
 import { ArrowRightSVG } from '@/svg';
 
-const ActionButton: FC<{ tutorial?: boolean }> = ({ tutorial }) => {
+import { ActionButtonProps } from './first-thing-first-section.types';
+
+const ActionButton: FC<ActionButtonProps> = ({ tutorial }) => {
   const t = useTranslations();
 
-  if (tutorial)
-    return (
-      <a href="https://youtu.be/czLQNJoQHBA" target="_blank" rel="noreferrer">
-        <Button
-          variant="text"
-          width={[
-            '-webkit-fill-available',
-            '-webkit-fill-available',
-            '-webkit-fill-available',
-            'unset',
-          ]}
-          display="flex"
-          justifyContent="center"
-          color="text"
-          textTransform="capitalize"
-        >
-          <ArrowRightSVG
-            width="1.25rem"
-            maxWidth="1.25rem"
-            height="1.25rem"
-            maxHeight="1.25rem"
-          />
-          <Typography variant="medium" color="primary">
-            {t('liquidity.firstThingFirst.tutorial')}
-          </Typography>
-        </Button>
-      </a>
-    );
-
   return (
-    <a href={Routes[RoutesEnum.Bridge]} target="_blank" rel="noreferrer">
+    <a
+      href={
+        tutorial ? 'https://youtu.be/czLQNJoQHBA' : Routes[RoutesEnum.Bridge]
+      }
+      target="_blank"
+      rel="noreferrer"
+    >
       <Button
-        variant="outline"
+        variant={tutorial ? 'text' : 'outline'}
         width={[
           '-webkit-fill-available',
           '-webkit-fill-available',
@@ -59,10 +38,13 @@ const ActionButton: FC<{ tutorial?: boolean }> = ({ tutorial }) => {
           maxHeight="1.25rem"
         />
         <Typography variant="medium" color="primary">
-          {t('liquidity.firstThingFirst.button')}
+          {tutorial
+            ? t('liquidity.firstThingFirst.buttonTutorial')
+            : t('liquidity.firstThingFirst.buttonAssets')}
         </Typography>
       </Button>
     </a>
   );
 };
+
 export default ActionButton;
