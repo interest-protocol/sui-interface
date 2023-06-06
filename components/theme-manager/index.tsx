9d3e8eb3fa351be3dd2bbb4ad3a3248ebf0f1c7e
git-marcopitra <marcomorete73@hotmail.com>
2023-06-06 04:54:53 +0100

    fix: insitutional colors

--- components/theme-manager/index.tsx
+++ components/theme-manager/index.tsx
@@ -15,6 +15,7 @@ import {
   DappGlobalStyles,
   LandingGlobalStyles,
 } from '@/design-system/global-styles';
+import institutionalTheme from '@/design-system/insitutianal-theme/dark';
 import { useLocalStorage } from '@/hooks';
 
 import LoadingPage from '../loading-page';
@@ -45,7 +46,7 @@ const Theme: FC<PropsWithChildren<ThemeProps>> = ({
 
   if (isInstitutional)
     return (
-      <InterestThemeProvider theme={{ setDark, ...darkTheme }}>
+      <InterestThemeProvider theme={{ setDark, ...institutionalTheme }}>
         <Global styles={LandingGlobalStyles} />
         {children}
       </InterestThemeProvider>
