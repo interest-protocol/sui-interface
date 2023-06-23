828768f3191e757465cd72d55cb42888e37625e8
git-marcopitra <marcomorete73@hotmail.com>
2023-06-23 03:57:50 +0100

    chore: imrpove solution

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,39 +1,42 @@
 import { GetStaticProps } from 'next';
-import dynamic from 'next/dynamic';
-import Error from 'next/error';
+import NotFoundPage from 'pages/404';
+import { mergeDeepRight } from 'ramda';
 
-import { LoadingPage } from '@/components';
+import { SEO } from '@/components';
 import { NextPageWithProps } from '@/interface';
+import ErrorPage from '@/views/dapp/v2/error';
 
-const Web3Manager = dynamic(() => import('@/components/web3-manager'), {
-  ssr: false,
-  loading: LoadingPage,
-});
+const DAppPage: NextPageWithProps = ({ now, messages, pageTitle }) => {
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production')
+    return (
+      <NotFoundPage messages={messages} now={now} pageTitle="common.error" />
+    );
 
-const Layout = dynamic(() => import('@/components/layout'), {
-  ssr: false,
-  loading: LoadingPage,
-});
-
-const NotFoundPage: NextPageWithProps = ({ pageTitle }) => (
-  <Web3Manager>
-    <Layout pageTitle={pageTitle}>
-      <Error statusCode={404} />
-    </Layout>
-  </Web3Manager>
-);
+  return (
+    <>
+      <SEO pageTitle={pageTitle} />
+      <ErrorPage />
+    </>
+  );
+};
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-  const messages = (await import(`../assets/messages/common/${locale}.json`))
-    .default;
+  const [commonMessages, lendingMessages] = await Promise.all([
+    import(`../assets/messages/common/${locale}.json`),
+    import(`../assets/messages/dapp/${locale}.json`),
+  ]);
 
+  const messages = mergeDeepRight(
+    commonMessages.default,
+    lendingMessages.default
+  );
   return {
     props: {
       messages,
       now: Date.now(),
-      pageTitle: 'common.error',
+      pageTitle: 'common.pageNotFound',
     },
   };
 };
 
-export default NotFoundPage;
+export default DAppPage;
