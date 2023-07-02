63c6ce3fe8cfa4a7bd9c1cefdfe4eecab840d2ea
Okusindja de Almeida <okusindja@gmail.com>
2023-07-02 19:57:57 +0100

    feat: add components

--- views/dapp/v2/components/layout/layout.types.ts
+++ views/dapp/v2/components/layout/layout.types.ts
@@ -1,4 +1,6 @@
+import { ReactNode } from 'react';
+
 export interface LayoutProps {
   dashboard?: boolean;
-  titlePage?: string;
+  titlePage?: ReactNode | string;
 }
--- views/dapp/v2/earn/index.tsx
+++ views/dapp/v2/earn/index.tsx
@@ -3,10 +3,12 @@ import { FC } from 'react';
 import { Layout } from '../components';
 import EarnCard from '../components/earn-card';
 import EarnContainer from '../components/earn-card/earn-container';
+import DetailedHeader from '../components/earn-detailed-header';
 
 const EarnPage: FC = () => {
   return (
-    <Layout dashboard titlePage="Earn">
+    // <Layout dashboard titlePage="Earn">
+    <Layout dashboard titlePage={<DetailedHeader />}>
       <EarnContainer columns={3}>
         <EarnCard isPool type="normal" />
         <EarnCard isPool type="normal" />
