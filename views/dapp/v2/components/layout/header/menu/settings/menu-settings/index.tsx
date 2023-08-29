9c1497918e4061d52c33e8964f37a8cef8dc61e1
Okusindja de Almeida <okusindja@gmail.com>
2023-08-29 13:29:56 +0100

    🚧 feat: wip

--- views/dapp/v2/components/layout/header/menu/settings/menu-settings/index.tsx
+++ views/dapp/v2/components/layout/header/menu/settings/menu-settings/index.tsx
@@ -4,11 +4,10 @@ import { FC } from 'react';
 // import { wrapperVariants } from '@/constants';
 // import { MenuSettingsProps } from '../menu.types';
 import MenuSettingsContent from './settings-list';
-import { MenuSettingsListProps } from './settings-list.types';
 
-const MenuSettings: FC<MenuSettingsListProps> = ({ setSettingsClosed }) => (
+const MenuSettings: FC = () => (
   <Motion textTransform="capitalize">
-    <MenuSettingsContent setSettingsClosed={setSettingsClosed} />
+    <MenuSettingsContent /*setSettingsClosed={setSettingsClosed}*/ />
   </Motion>
 );
 
