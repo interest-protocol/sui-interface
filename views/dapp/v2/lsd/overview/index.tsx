f5d0a62d4e6c67f640c64b331bac1e8606077d8d
Kipas Jr <34079870+KipandaJr@users.noreply.github.com>
2023-09-04 05:39:57 +0100

    feat: :construction: your info section added (#439)

--- views/dapp/v2/lsd/overview/index.tsx
+++ views/dapp/v2/lsd/overview/index.tsx
@@ -2,7 +2,11 @@ import { Box } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
 const Overview: FC = () => (
-  <Box bg="onSurface" width={['100%', '100%', '100%', '55%']}>
+  <Box
+    bg="onSurface"
+    height="max-content"
+    width={['100%', '100%', '100%', '55%']}
+  >
     Overview
   </Box>
 );
