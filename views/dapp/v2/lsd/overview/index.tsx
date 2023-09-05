fb075ed92ff0c15f4061ad79af235d51553e0347
Kipas Jr <34079870+KipandaJr@users.noreply.github.com>
2023-09-05 22:35:17 +0100

    sui prices market (#440)
    * feat: :construction: price market implemented
    
    * feat: :construction: review changes
    
    * feat: :construction: adding props

--- views/dapp/v2/lsd/overview/index.tsx
+++ views/dapp/v2/lsd/overview/index.tsx
@@ -1,14 +1,34 @@
+import { Network } from '@interest-protocol/sui-amm-sdk';
 import { Box } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
-const Overview: FC = () => (
-  <Box
-    bg="onSurface"
-    height="max-content"
-    width={['100%', '100%', '100%', '55%']}
-  >
-    Overview
-  </Box>
-);
+import { COINS } from '@/constants';
+
+import { DATA_CHART } from './overview.data';
+import { SuiPriceInfoProps, TrendInfoProps } from './overview.type';
+import PricesMarketCard from './prices-market-card';
+
+const Overview: FC = () => {
+  const trendInfo: TrendInfoProps = {
+    percentage: 3.4,
+    isTrendUp: true,
+    daysPast: 45,
+  };
+
+  const priceInfo: SuiPriceInfoProps = {
+    coin: COINS[Network.TESTNET].SUID,
+    amount: 0.943,
+  };
+
+  return (
+    <Box width={['100%', '100%', '100%', '55%']}>
+      <PricesMarketCard
+        trendInfo={trendInfo}
+        priceInfo={priceInfo}
+        chartData={DATA_CHART}
+      />
+    </Box>
+  );
+};
 
 export default Overview;
---
+++ views/dapp/v2/lsd/overview/prices-market-card/index.tsx
@@ -0,0 +1,43 @@
+import { Box, Theme, Typography, useTheme } from '@interest-protocol/ui-kit';
+import { useTranslations } from 'next-intl';
+import { FC } from 'react';
+
+import { SuiPriceMarketProps } from '../overview.type';
+import PriceMarketChart from './price-market-chart';
+import SuiPriceInfo from './sui-price-info';
+import TrendInfo from './trend-info';
+
+const PricesMarketCard: FC<SuiPriceMarketProps> = ({
+  priceInfo,
+  trendInfo,
+  chartData,
+}) => {
+  const t = useTranslations();
+  const { dark } = useTheme() as Theme;
+  return (
+    <Box bg="surface.container" p="l" borderRadius="0.5rem">
+      <Typography
+        variant="extraSmall"
+        fontSize="0.688rem"
+        color={dark ? 'white' : 'black'}
+        mb="l"
+        textTransform="capitalize"
+      >
+        {t('lsd.cards.priceMarket.title')}
+      </Typography>
+      <Box
+        display="flex"
+        alignItems="flex-end"
+        flexWrap="wrap"
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <SuiPriceInfo {...priceInfo} />
+        <TrendInfo {...trendInfo} />
+        <PriceMarketChart data={chartData} />
+      </Box>
+    </Box>
+  );
+};
+
+export default PricesMarketCard;
