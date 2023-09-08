9e417c4a25f00feb6a0171b8384dfeb682a9c501
Okusindja de Almeida <okusindja@gmail.com>
2023-09-08 19:15:46 +0100

    ✨ feat: add overview

--- views/dapp/v2/lsd/index.tsx
+++ views/dapp/v2/lsd/index.tsx
@@ -1,39 +1,50 @@
-import { Box, Typography } from '@interest-protocol/ui-kit';
+import { Box, Tabs, Typography } from '@interest-protocol/ui-kit';
 import { useTranslations } from 'next-intl';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { Layout } from '../components';
 import LSDHeader from './lsd-header';
+import Overview from './overview';
 import StakingForm from './staking-form';
 import Statistics from './statistics';
 
 const LSD: FC = () => {
   const t = useTranslations();
+  const [changeTab, setChangeTab] = useState<number>(0);
 
   return (
     <Layout dashboard titlePage={<LSDHeader />}>
-      <Box variant="container" display="flex" flexDirection="column">
-        <Box
-          pb="1rem"
-          width="100%"
-          gridColumn="1/-1"
-          display="flex"
-          flexDirection={['column', 'column', 'column', 'row']}
-          gap={['l', 'l', 'l', '3xl']}
-        >
-          <Typography
-            display={['block', 'block', 'block', 'none']}
-            variant="displayLarge"
-            color="onSurface"
-            textTransform="capitalize"
-            textAlign="center"
+      <Tabs
+        items={['Stake', 'Stats']}
+        defaultTabIndex={changeTab}
+        onChangeTab={(changeTab) => setChangeTab(changeTab)}
+      />
+      {changeTab === 0 ? (
+        <Box variant="container" display="flex" flexDirection="column">
+          <Box
+            pb="1rem"
+            width="100%"
+            gridColumn="1/-1"
+            display="flex"
+            flexDirection={['column', 'column', 'column', 'row']}
+            gap={['l', 'l', 'l', '3xl']}
           >
-            {t('lsd.metadata.title')}
-          </Typography>
-          <Statistics />
-          <StakingForm />
+            <Typography
+              display={['block', 'block', 'block', 'none']}
+              variant="displayLarge"
+              color="onSurface"
+              textTransform="capitalize"
+              textAlign="center"
+            >
+              {t('lsd.metadata.title')}
+            </Typography>
+            <Statistics />
+            <StakingForm />
+          </Box>
         </Box>
-      </Box>
+      ) : (
+        <Overview />
+      )}
     </Layout>
   );
 };
