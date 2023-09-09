341a161495ce8b09dbabcb0ec1209ad9cd1782af
Okusindja de Almeida <okusindja@gmail.com>
2023-09-09 04:13:21 +0100

    ✨ feat: add total staked to stats section

--- views/dapp/v2/lsd/stats-section/index.tsx
+++ views/dapp/v2/lsd/stats-section/index.tsx
@@ -2,11 +2,13 @@ import { Box } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
 import Overview from './overview';
+import TotalStaked from './total-staked';
 
 const StatsSection: FC = () => {
   return (
     <Box variant="container" display="flex" flexDirection="column">
-      <Overview />;
+      <Overview />
+      <TotalStaked />
     </Box>
   );
 };
