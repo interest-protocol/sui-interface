5d561dacfb0c1283fba55c0da06ff8d3afbc2ac7
git-marcopitra <marcomorete73@hotmail.com>
2023-09-13 07:26:30 +0100

    chore: wip

--- views/dapp/v2/lst/validators/all-validators/index.tsx
+++ views/dapp/v2/lst/validators/all-validators/index.tsx
@@ -1,21 +1,17 @@
 import { Box } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
-import { useGetValidatorTableFields } from '../../lst.hooks';
 import { AllValidatorsProps } from './all-validators.types';
 import ValidatorSearch from './validator-search';
 import ValidatorsTable from './validators-table';
 
 const AllValidators: FC<AllValidatorsProps> = ({ activeValidators }) => {
-  const { data: validatorTable, isLoading: isValidatorTableLoading } =
-    useGetValidatorTableFields();
-
-  console.log({ validatorTable, isValidatorTableLoading, activeValidators });
+  console.log({ activeValidators });
 
   return (
     <Box bg="surface.container" p="l" borderRadius="0.5rem">
       <ValidatorSearch />
-      <ValidatorsTable />
+      <ValidatorsTable activeValidators={activeValidators} />
     </Box>
   );
 };
--- views/dapp/v2/lst/validators/all-validators/validators-table/index.tsx
+++ views/dapp/v2/lst/validators/all-validators/validators-table/index.tsx
@@ -1,10 +1,16 @@
-import { Box } from '@interest-protocol/ui-kit';
+import { Box, Typography } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
+import { v4 } from 'uuid';
 
-import ValidatorsTableBody from './validators-table-body';
+import { SUISVG } from '@/components/svg/v2';
+
+import TableRow from '../../../components/table-row';
+import { AllValidatorsProps } from '../all-validators.types';
 import ValidatorsTableHead from './validators-table-head';
 
-const ValidatorsTable: FC = () => {
+const ValidatorsTable: FC<AllValidatorsProps> = ({ activeValidators }) => {
+  console.log({ activeValidators });
+
   return (
     <Box
       width="100%"
@@ -19,7 +25,56 @@ const ValidatorsTable: FC = () => {
       <Box minWidth="55em">
         <ValidatorsTableHead />
         <Box>
-          <ValidatorsTableBody />
+          {activeValidators.map(({ name, imageUrl }, index) => (
+            <Box key={v4()}>
+              <TableRow numCols={5}>
+                <Typography variant="small">{index + 1}</Typography>
+                <Box display="flex" gap="m" alignItems="center">
+                  <Box display="flex">
+                    <Box
+                      width="2rem"
+                      height="2rem"
+                      borderRadius="0.25rem"
+                      backgroundColor="white"
+                      backgroundSize="contain"
+                      backgroundPosition="center center"
+                      backgroundImage={`url(${imageUrl})`}
+                    />
+                  </Box>
+                  <Typography variant="medium">{name}</Typography>
+                </Box>
+                <Box display="flex" justifyContent="flex-end">
+                  <Box display="flex" alignItems="center" gap="0.5rem">
+                    <Typography variant="small" textAlign="center">
+                      41.47 M
+                    </Typography>
+                    <Box
+                      bg="#6FBCF0"
+                      width="1rem"
+                      color="white"
+                      height="1rem"
+                      display="flex"
+                      borderRadius="full"
+                      justifyContent="center"
+                    >
+                      <SUISVG
+                        maxHeight="1rem"
+                        maxWidth="1rem"
+                        width="100%"
+                        height="100%"
+                      />
+                    </Box>
+                  </Box>
+                </Box>
+                <Typography variant="small" textAlign="right">
+                  4.95%
+                </Typography>
+                <Typography variant="small" textAlign="right">
+                  4.95%
+                </Typography>
+              </TableRow>
+            </Box>
+          ))}
         </Box>
       </Box>
     </Box>
