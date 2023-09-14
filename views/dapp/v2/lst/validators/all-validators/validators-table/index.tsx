c28ee50e903246f4d622aa3082d99c979aa0afc9
git-marcopitra <marcomorete73@hotmail.com>
2023-09-14 03:13:54 +0100

    fix: validators data

--- views/dapp/v2/lst/validators/all-validators/validators-table/index.tsx
+++ views/dapp/v2/lst/validators/all-validators/validators-table/index.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 import { v4 } from 'uuid';
 
 import { SUISVG } from '@/components/svg/v2';
+import { formatMoney } from '@/utils';
 
 import TableRow from '../../../components/table-row';
 import { AllValidatorsProps } from '../all-validators.types';
@@ -25,60 +26,71 @@ const ValidatorsTable: FC<AllValidatorsProps> = ({ activeValidators }) => {
       <Box minWidth="55em">
         <ValidatorsTableHead />
         <Box>
-          {activeValidators.map(({ name, imageUrl, projectUrl }, index) => (
-            <Box
-              key={v4()}
-              cursor="pointer"
-              onClick={() => window.open(projectUrl)}
-            >
-              <TableRow numCols={5}>
-                <Typography variant="small">{index + 1}</Typography>
-                <Box display="flex" gap="m" alignItems="center">
-                  <Box display="flex">
-                    <Box
-                      width="2rem"
-                      height="2rem"
-                      borderRadius="0.25rem"
-                      backgroundColor="white"
-                      backgroundSize="contain"
-                      backgroundPosition="center center"
-                      backgroundImage={`url(${imageUrl})`}
-                    />
-                  </Box>
-                  <Typography variant="medium">{name}</Typography>
-                </Box>
-                <Box display="flex" justifyContent="flex-end">
-                  <Box display="flex" alignItems="center" gap="0.5rem">
-                    <Typography variant="small" textAlign="center">
-                      41.47 M
-                    </Typography>
-                    <Box
-                      bg="#6FBCF0"
-                      width="1rem"
-                      color="white"
-                      height="1rem"
-                      display="flex"
-                      borderRadius="full"
-                      justifyContent="center"
-                    >
-                      <SUISVG
-                        maxHeight="1rem"
-                        maxWidth="1rem"
-                        width="100%"
-                        height="100%"
+          {activeValidators.map(
+            (
+              {
+                name,
+                imageUrl,
+                projectUrl,
+                commissionRate,
+                stakingPoolSuiBalance,
+              },
+              index
+            ) => (
+              <Box
+                key={v4()}
+                cursor="pointer"
+                onClick={() => window.open(projectUrl)}
+              >
+                <TableRow numCols={5}>
+                  <Typography variant="small">{index + 1}</Typography>
+                  <Box display="flex" gap="m" alignItems="center">
+                    <Box display="flex">
+                      <Box
+                        width="2rem"
+                        height="2rem"
+                        borderRadius="0.25rem"
+                        backgroundColor="white"
+                        backgroundSize="contain"
+                        backgroundPosition="center center"
+                        backgroundImage={`url(${imageUrl})`}
                       />
                     </Box>
+                    <Typography variant="medium">{name}</Typography>
                   </Box>
-                </Box>
-                <Typography variant="small" textAlign="right">
-                  4.95%
-                </Typography>
-                <Typography variant="small" textAlign="right">
-                  4.95%
-                </Typography>
-              </TableRow>
-            </Box>
-          ))}
+                  <Box display="flex" justifyContent="flex-end">
+                    <Box display="flex" alignItems="center" gap="0.5rem">
+                      <Typography variant="small" textAlign="center">
+                        {formatMoney(stakingPoolSuiBalance / 10e9)}
+                      </Typography>
+                      <Box
+                        bg="#6FBCF0"
+                        width="1rem"
+                        color="white"
+                        height="1rem"
+                        display="flex"
+                        borderRadius="full"
+                        justifyContent="center"
+                      >
+                        <SUISVG
+                          maxHeight="1rem"
+                          maxWidth="1rem"
+                          width="100%"
+                          height="100%"
+                        />
+                      </Box>
+                    </Box>
+                  </Box>
+                  <Typography variant="small" textAlign="right">
+                    {0}%
+                  </Typography>
+                  <Typography variant="small" textAlign="right">
+                    {commissionRate / 1000}%
+                  </Typography>
+                </TableRow>
+              </Box>
+            )
+          )}
         </Box>
       </Box>
     </Box>
