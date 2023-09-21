c60791eaeeb2b763f298e22d9629f60fef669bd9
git-marcopitra <marcomorete73@hotmail.com>
2023-09-21 08:28:27 +0100

    fix: lst unstake modal

--- views/dapp/v2/lst/staked/staking-form/your-info/modal/line-wrapper.tsx
+++ views/dapp/v2/lst/staked/staking-form/your-info/modal/line-wrapper.tsx
@@ -9,58 +9,56 @@ const LineWrapper: FC<PropsWithChildren<LineWrapperProps>> = ({
   token,
   reverse,
   children,
-}) => {
-  return (
-    <Box>
-      <Typography
-        variant="extraSmall"
-        fontWeight="400"
-        fontSize="0.6875rem"
-        color="onSurface"
-        opacity={0.6}
-        mb="0.5rem"
-        textTransform="capitalize"
-      >
-        {title}
-      </Typography>
-      <Box
-        p="0.75rem"
-        mb="l"
-        borderRadius="0.25rem"
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        bg="surface.containerHighest"
-      >
-        <Box display="flex" gap="1rem" alignItems="center">
-          {Icon}
-          <Box
-            display="flex"
-            flexDirection={reverse ? 'column-reverse' : 'column'}
+}) => (
+  <Box>
+    <Typography
+      variant="extraSmall"
+      fontWeight="400"
+      fontSize="0.6875rem"
+      color="onSurface"
+      opacity={0.6}
+      mb="0.5rem"
+      textTransform="capitalize"
+    >
+      {title}
+    </Typography>
+    <Box
+      p="0.75rem"
+      mb="l"
+      borderRadius="0.25rem"
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      bg="surface.containerHighest"
+    >
+      <Box display="flex" gap="1rem" alignItems="center">
+        {Icon}
+        <Box
+          display="flex"
+          flexDirection={reverse ? 'column-reverse' : 'column'}
+        >
+          <Typography
+            variant="small"
+            fontWeight="400"
+            fontSize="1rem"
+            color="onSurface"
           >
-            <Typography
-              variant="small"
-              fontWeight="400"
-              fontSize="1rem"
-              color="onSurface"
-            >
-              {token.main}
-            </Typography>
-            <Typography
-              variant="extraSmall"
-              fontWeight="400"
-              fontSize="0.6875rem"
-              color="onSurface"
-              opacity="0.6"
-            >
-              {token.secondary}
-            </Typography>
-          </Box>
+            {token.main}
+          </Typography>
+          <Typography
+            variant="extraSmall"
+            fontWeight="400"
+            fontSize="0.6875rem"
+            color="onSurface"
+            opacity="0.6"
+          >
+            {token.secondary}
+          </Typography>
         </Box>
-        {children}
       </Box>
+      {children}
     </Box>
-  );
-};
+  </Box>
+);
 
 export default LineWrapper;
