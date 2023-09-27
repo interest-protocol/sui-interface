e7578807978281510d6827dd6aadc881716cefe9
Okusindja de Almeida <okusindja@gmail.com>
2023-09-27 04:47:28 +0100

    ✨ feat: add validators flow

--- elements/textarea/index.ts
+++ elements/textarea/index.ts
@@ -1,10 +1,7 @@
 import stylin from '@stylin.js/react';
-import { PropsWithChildren } from 'react';
 
 import { BoxElementProps } from '../box/box.types';
 
-const Textarea = stylin<BoxElementProps>(
-  'textarea'
-)();
+const Textarea = stylin<BoxElementProps>('textarea')();
 
 export default Textarea;
