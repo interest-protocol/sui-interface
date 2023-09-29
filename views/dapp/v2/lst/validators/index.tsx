957e4551ca39d0f47193e39a317338f1bfb5caa2
git-marcopitra <marcomorete73@hotmail.com>
2023-09-29 02:15:52 +0100

    build: final fix

--- views/dapp/v2/lst/validators/index.tsx
+++ views/dapp/v2/lst/validators/index.tsx
@@ -2,11 +2,12 @@ import { Box } from '@interest-protocol/ui-kit';
 import { useTranslations } from 'next-intl';
 import { FC } from 'react';
 
-import { SUISVG, UsersSVG } from '@/components/svg/v2';
 import { FixedPointMath } from '@/lib';
 
 import Overview from '../components/overview';
 import { useLstData } from '../lst.hooks';
+import { DERIVATED_SUI_SYMBOL } from '../lst.types';
+import { OverviewItem } from '../stats/stats.type';
 import AllValidators from './all-validators';
 
 const OverViewWrapper: FC = () => {
@@ -15,21 +16,21 @@ const OverViewWrapper: FC = () => {
   const { activeValidators, iSuiExchangeRate, isLoading, lstStorage } =
     useLstData();
 
-  const OVERVIEW_DATA = [
+  const OVERVIEW_DATA: ReadonlyArray<OverviewItem> = [
     {
       description: 'lst.overview.totalSuiStaked',
-      Icon: SUISVG,
+      type: 'SUI' as DERIVATED_SUI_SYMBOL,
       // Sui has 9 decimals
       value: FixedPointMath.toNumber(lstStorage.totalPrincipal),
     },
     {
       description: 'lst.exchangeRate',
-      Icon: UsersSVG,
+      type: 'users',
       value: iSuiExchangeRate,
     },
     {
+      type: 'users',
       description: 'lst.overview.validators',
-      Icon: UsersSVG,
       value: activeValidators?.length || 0,
     },
   ];
