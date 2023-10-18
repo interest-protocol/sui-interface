1f74060f27eba63ae63f82f7c0aa3ef88f190992
Okusindja de Almeida <91469357+okusindja@users.noreply.github.com>
2023-10-18 21:16:10 +0100

    🐛 bugfix: bonds stake translations (#519)

--- views/dapp/v2/lst/bonds/stake/stake-form/stake-validator/index.tsx
+++ views/dapp/v2/lst/bonds/stake/stake-form/stake-validator/index.tsx
@@ -1,5 +1,6 @@
 import { Box, RadioButton, Typography } from '@interest-protocol/ui-kit';
 import {} from 'framer-motion';
+import { useTranslations } from 'next-intl';
 import { not } from 'ramda';
 import { FC, useEffect, useState } from 'react';
 import { useWatch } from 'react-hook-form';
@@ -11,6 +12,7 @@ import { useBondsContext } from '../../../bonds.hooks';
 import ValidatorSelector from './validator-selector';
 
 const StakeValidator: FC = () => {
+  const t = useTranslations();
   const { network } = useNetwork();
   const {
     form: { setValue, control },
@@ -50,7 +52,9 @@ const StakeValidator: FC = () => {
               setValue('validatorType', !isValidatorAuto ? 'auto' : 'none');
             }}
           />
-          <Typography variant="medium">Automated</Typography>
+          <Typography variant="medium">
+            {t('lst.bonds.stake.form.automated')}
+          </Typography>
         </Box>
         <Box display="flex" gap="m" alignItems="center">
           <RadioButton
@@ -61,7 +65,9 @@ const StakeValidator: FC = () => {
               setValue('validatorType', !isValidatorManual ? 'manual' : 'none');
             }}
           />
-          <Typography variant="medium">Manual</Typography>
+          <Typography variant="medium">
+            {t('lst.bonds.stake.form.manual')}
+          </Typography>
         </Box>
       </Box>
       {isValidatorManual && <ValidatorSelector />}
